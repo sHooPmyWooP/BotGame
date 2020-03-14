1f840408e79087d6e3a1083713fc8c347ca3b0f2
David <david-achilles@hotmail.de>
2020-03-14 17:28:45 +0100

    OGame API sqlite3 - update and commentary

--- Classes/OGame_API.py
+++ Classes/OGame_API.py
@@ -1,5 +1,6 @@
 import sqlite3
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
 import requests
 
@@ -7,7 +8,21 @@ from Classes.PlanetEnemy import PlanetEnemy
 
 
 class OGameAPI:
+    """
+    Interact with OGames API: http://s{server_number}-{language}.ogame.gameforge.com/api/{endpoint}
+    Endpoints:
+    players.xml (1 day): Contain list of all players on the server along with their names and status.
+    alliances.xml (1 day): Contain list of all alliances on the server along with their members, name and tag.
+    serverData.xml (1 day): Contain information about server properties such as name, language, speed, debris etc.
+    universe.xml (1 week): Contain list of all planets on the server along with their names, coordinates and owners.
+    highscore.xml (1 hour): Contain statistics of various types about players and alliances on the server.
+    """
+
     def __init__(self, server_number, language):
+        """
+        :param server_number: int
+        :param language: str
+        """
         self.server_number = server_number
         self.language = language
         self.planets = []
@@ -15,6 +30,10 @@ class OGameAPI:
         self.planets_players = []
 
     def get_planets(self):
+        """
+        Get all planets from server updated to self.planets
+        :return: None
+        """
         self.planets = []
         endpoint = 'universe.xml'
         response = requests.get(f'http://s{self.server_number}-{self.language}.ogame.gameforge.com/api/{endpoint}').text
@@ -25,6 +44,10 @@ class OGameAPI:
             self.planets.append(planet_format)
 
     def get_players(self):
+        """
+        Get all players from server updated to self.players
+        :return: None
+        """
         self.players = []
         endpoint = 'players.xml'
         response = requests.get(f'http://s{self.server_number}-{self.language}.ogame.gameforge.com/api/{endpoint}').text
@@ -36,6 +59,10 @@ class OGameAPI:
             self.players.append(player_format)
 
     def map_players_to_planet(self):
+        """
+        Join Planets with Players and thus get the ability to e.g. filter for inactive targets
+        :return: None
+        """
         self.get_planets()
         self.get_players()
         # Map Players to Planets
@@ -46,20 +73,27 @@ class OGameAPI:
                     self.planets_players.append(PlanetEnemy(planet[1], player[0], player[1]))
 
     def push_inactive_to_db(self):
+        """
+        Filter Planets for Inactive PLayers and push those to inactive_players.db.
+        Tables in that DB: INACTIVE_PLAYERS (server_number, player_id, player_status, planet_coords, timestamp)
+        :return: None
+        """
         self.map_players_to_planet()
-        conn = sqlite3.connect('Resources\db\inactive_player.db')
+        conn = sqlite3.connect('Resources\db\inactive_players.db')
         c = conn.cursor()
         c.execute("""
         CREATE TABLE IF NOT EXISTS INACTIVE_PLAYERS(
         server_number integer,
         player_id integer,
         player_status text,
-        planet_coords text);
+        planet_coords text,
+        timestamp datetime default current_timestamp);
         """)
-
+        current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         for planet in self.planets_players:
             if planet.status == "I" or planet.status == "i":
-                c.execute(f"INSERT INTO INACTIVE_PLAYERS VALUES ('{self.server_number}', '{planet.player_id}',"
-                          f" '{planet.status}', '{planet.coords}');")
+                statement = "INSERT INTO 'INACTIVE_PLAYERS' VALUES (?, ?, ?,?,?);"
+                tuple = (self.server_number, planet.player_id, planet.status, planet.coords, current_timestamp)
+                c.execute(statement, tuple)
         conn.commit()
         conn.close()
